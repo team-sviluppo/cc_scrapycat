8da76cc6ece32927f80b4e131dd19f77d9935bff
Davide Modolo <36373601+davidemodolo@users.noreply.github.com>
2025-08-22 15:13:51 +0200

    max_depth from 0 to -1 to keep old behavior by default

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ from enum import Enum
 class PluginSettings(BaseModel):
     ingest_pdf: bool = False
     skip_get_params: bool = False
-    max_depth: int = 0
+    max_depth: int = -1
     max_pages: int = -1
     allowed_extra_roots: str = ""  # Comma-separated list of allowed root URLs
 
